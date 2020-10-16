130b0805e400a385f9e0ee80c98d1892e0a63b20
Mohit Kumar Nayak <nayakmohit08@gmail.com>
2020-10-16 17:28:26 -0400

    HeartBeat Test

--- HeartBeat/HeartBeat.java
+++ HeartBeat/HeartBeat.java
@@ -18,7 +18,7 @@ public class HeartBeat
         {
             if (text != currentIp)
             {
-                System.out.println("172.31.27.138" == currentIp);
+                System.out.println(text.length());
                 ip.add(text);
             }
         }
